8fb6be801d252845164a6c9eb27e5544094c30a8
akjagadish <akshaykjagadish@gmail.com>
2026-01-04 10:34:42 -0500

    run on cpu

--- bash/run_gecco.sh
+++ bash/run_gecco.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
-#SBATCH --partition=ailab
+## SBATCH --partition=ailab
 #SBATCH -J gecco
 #SBATCH -N 1
 #SBATCH --error=logs/%x-%j.err
 #SBATCH --output=logs/%x-%j.out
-#SBATCH --gres=gpu:1
-#SBATCH --mem=400G
+## SBATCH --gres=gpu:1
+#SBATCH --mem=40G
 #SBATCH -t 04:00:00
 #SBATCH -c 32
 #SBATCH --mail-user=akshay.jagadish@princeton.edu
@@ -13,5 +13,6 @@
 cd ~/gecco-1/
 module purge
 module load anaconda3/2025.12 
+module load proxy/default
 conda activate gecco
 python scripts/two_step_individual_stai_class.py

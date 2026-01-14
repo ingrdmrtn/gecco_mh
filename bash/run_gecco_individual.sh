94337b7967ab4516799dafcab8315e9ab0d66383
akjagadish <akshaykjagadish@gmail.com>
2026-01-13 19:49:07 -0500

    scripts to run group and individual models

---
+++ bash/run_gecco_individual.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+## SBATCH --partition=ailab
+#SBATCH -J gecco
+#SBATCH -N 1
+#SBATCH --error=logs/%x-%j.err
+#SBATCH --output=logs/%x-%j.out
+## SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#SBATCH -t 04:00:00
+#SBATCH -c 32
+#SBATCH --mail-user=akshay.jagadish@princeton.edu
+
+cd ~/gecco-1/
+module purge
+module load anaconda3/2025.12 
+module load proxy/default
+conda activate gecco
+python scripts/two_step_individual_function.py

59744d9aae2714dc8b9ae034f9815282600f5f04
akjagadish <akshaykjagadish@gmail.com>
2026-01-16 11:56:28 -0500

    script to run max settings

--- bash/run_gecco_individual.sh
+++ bash/run_gecco_individual.sh
@@ -15,5 +15,7 @@ module purge
 module load anaconda3/2025.12 
 module load proxy/default
 conda activate gecco
-python scripts/two_step_individual_function.py --config two_step_psychiatry_individual_ocd_function_gemini-3-pro_ocd.yaml
-python scripts/two_step_individual_function.py --config two_step_psychiatry_individual_function_gemini-3-pro_ocd.yaml
+
+python scripts/two_step_individual_function.py --config two_step_psychiatry_individual_function_gemini-3-pro_ocd_maxsetting.yaml
+# python scripts/two_step_individual_function.py --config two_step_psychiatry_individual_ocd_function_gemini-3-pro_ocd.yaml
+# python scripts/two_step_individual_function.py --config two_step_psychiatry_individual_function_gemini-3-pro_ocd.yaml

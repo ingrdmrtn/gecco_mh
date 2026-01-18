ac9aea4ebf522ef42092159348038953aed9c74e
akjagadish <akshaykjagadish@gmail.com>
2026-01-18 13:04:47 -0500

    add verification loop for extration of parameters and mechanism from participant specific models, add additional results into the same csv such as performance, parsed params for reference, and fitted parameters

--- analysis/two_step_task/compare_gecco_baseline.py
+++ analysis/two_step_task/compare_gecco_baseline.py
@@ -1,3 +1,8 @@
+import os
+import sys
+from regex import match
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "..")))
 from gecco.load_llms.model_loader import load_llm
 from gecco.utils import *
 from gecco.offline_evaluation.evaluation_functions import aic as _aic, bic as _bic
@@ -13,14 +18,6 @@ import glob
 import numpy as np
 import pandas as pd
 import ipdb
-import os
-import sys
-
-from regex import match
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "..", "..")))
-
-
 # from data.ocd.preprocess_data import hybrid_model
 
 
@@ -68,18 +65,20 @@ metric_map = {"AIC": _aic, "BIC": _bic}
 metric_func = metric_map.get(metric_name, _bic)
 
 best_models = f'{project_root}/results/{cfg.task.name}_{cfg.evaluation.fit_type}/models/'
+bics = f'{project_root}/results/{cfg.task.name}_{cfg.evaluation.fit_type}/bics/'
 best_simulated_models = f'{project_root}/results/{cfg.task.name}_{cfg.evaluation.fit_type}/simulation/'
 simulation_columns = cfg.data.simulation_columns
 param_dir = project_root / \
     f"results/{cfg.task.name}_{cfg.evaluation.fit_type}/parameters/"
 
-best_params_list = {'participant': [], 'parameters': [],
-                    'param_names': [], 'num_params': [],
+best_params_list = {'participant': [], 'fitted_parameters': [],
+                    'parsed_params': [], 'num_params': [], 'extraction_mismatch': [],
                     'shared_params': [], 'unique_params': [],
                     'shared_mechanisms': [], 'unique_mechanisms': [],
-                    'shared': [], 'model_type': []}
-
-for p in participants[14:]:
+                    'shared': [], 'model_type': [], 'baseline_params': [],
+                    'baseline_bic': [], 'best_model_bic': []}
+baseline_params = ['learning_rate', 'learning_rate_2', 'beta', 'beta_2', 'w', 'lambd', 'perseveration']
+for p in participants[14:39]:
 
     print(p)
     df_participant = df[df.participant == p].reset_index()
@@ -91,30 +90,55 @@ for p in participants[14:]:
     vals = {'base_code': compare_config.compare.hybrid_model, 'model_code': best_model,
             'psychiatry': "- OCD: obsesstive compulsive disorder score (0-1) modulates behavior" if psychiatry else ""}
     prompt = compare_config.compare.comparison_prompt.format(**vals)
-
     model, tokenizer = load_llm(cfg.llm.provider, cfg.llm.base_model)
     search = GeCCoModelSearch(model, tokenizer, cfg, df_participant, None)
-    output = search.generate(model, tokenizer, prompt)
-    print(output)
-    model_outputs = save_llm_json(output)
-
+    retry_count = 0
+    while True:
+        output = search.generate(model, tokenizer, prompt)
+        print(output)
+        model_outputs = save_llm_json(output)
+        # if baseline_params and participant_model_parms match with regex'ed output, break
+        llm_extracted_model_parameter_names = model_outputs["participant_model_parameter_names"]
+        llm_extracted_base_model_parameter_names = model_outputs["base_model_parameter_names"]
+        # retry if baseline_params do not match
+        if set(baseline_params) == set(llm_extracted_base_model_parameter_names):
+            print('Baseline model parameters match expected parameters.')
+            best_params_list['extraction_mismatch'].append(False)
+            break
+        if retry_count >= 3:
+            print(
+                f'Max retries reached for participant {p}. Proceeding with current LLM output despite mismatch in baseline parameters.')
+            best_params_list['extraction_mismatch'].append(True)
+            break
+        else:
+            prompt = prompt + "\n\nThe previous output did not match the expected baseline model parameters. Please ensure that the baseline model parameters are correctly parsed. Try again."
+            print(
+                f'Retrying LLM output for participant {p} due to mismatch in baseline parameters.') 
+            retry_count += 1
     try:
         # print(param_dir)
-        best_parameters = pd.read_csv(
+        best_model_parameters = pd.read_csv(
             f'{param_dir}/best_params_run0_participant{p}.csv')
+        fitted_parameters = [best_model_parameters[n][0] for n in best_model_parameters.columns]
     except:  # noqa: E722
         print(f'No parameters for participant {p}')
         continue
-
-    # parameter_names = extract_parameter_names(participant_simulation_model)
+    
+    # load bics 
+    bic_path = f'{bics}best_bic_0_participant{p}.json'
+    # load json from bic_path
+    bic = json.load(open(bic_path, 'r'))
+    # get baseline model bic
+    best_model_bic = bic['bic']
+    # baseline bic from df_participant
+    baseline_bic = df_participant['baseline_bic'].iloc[0]
 
     # if oci in  parameter_names:
-    parameters = [best_parameters[n][0] for n in best_parameters.columns]
-    best_params_list['param_names'].append(
-        best_parameters.columns.values)
     best_params_list['participant'].append(p)
-    best_params_list['parameters'].append(parameters)
-    best_params_list['num_params'].append(len(parameters))
+    best_params_list['parsed_params'].append(
+        best_model_parameters.columns.values)
+    best_params_list['fitted_parameters'].append(fitted_parameters)
+    best_params_list['num_params'].append(len(fitted_parameters))
     best_params_list['shared_params'].append(
         model_outputs['shared_parameters'])
     best_params_list['unique_params'].append(
@@ -125,7 +149,13 @@ for p in participants[14:]:
         model_outputs['unique_mechanisms'])
     best_params_list['shared'].append(model_outputs['shared'])
     best_params_list['model_type'].append(model_outputs['model_type'])
+    best_params_list['baseline_params'].append(baseline_params)
+    best_params_list['baseline_bic'].append(baseline_bic)
+    best_params_list['best_model_bic'].append(best_model_bic)
 
-pooled_best_params = pd.DataFrame(best_params_list)
-pooled_best_params.to_csv(
-    f'{project_root}/analysis/two_step_task/pooled_params_{cfg.task.name}_{cfg.evaluation.fit_type}_oci.csv')
+# check if dataframe ecists append to existing else create new
+best_params_df = pd.DataFrame(best_params_list)
+best_params_df.to_csv(
+    f'{project_root}/results/{cfg.task.name}_{cfg.evaluation.fit_type}/comparison/gecco_baseline_comparison.csv', index=False)
+print(
+        f'Saved comparison results to {project_root}/results/{cfg.task.name}_{cfg.evaluation.fit_type}/comparison/gecco_baseline_comparison.csv')
